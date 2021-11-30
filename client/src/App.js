0d42cca01b7432c6c596ade1b9112bb517ff80df
Biswajit Rout <b119018@iiit-bh.ac.in>
2021-12-01 03:29:54 +0530

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { getPosts } from './actions/posts';
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import devstagram from './images/devstagram.png';
-import useStyles from './styles';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
+
 const App = () => {
-    const [currentId, setCurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(getPosts()); //we are triggering an action creator then from there we fetch our posts and then using dispatch we trigger our reducer which will return new state
-    },[currentId, dispatch])
     return (
-        <Container maxWidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">DevstaGram</Typography>
-                <img className={classes.image} src={devstagram} alt="devstagram" height="60" width="50"></img>
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container className={classes.mainContainer} justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId}/>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
+        <BrowserRouter>
+            <Container maxWidth="lg">
+                <Navbar />
+                <Routes>
+                    <Route path="/" exact element={<Home/>} />
+                    <Route path="/auth" exact component={<Auth/>} />
+                </Routes>
+            </Container>
+        </BrowserRouter>
     );
 }
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+
 import { Provider } from 'react-redux';  //provider keeps track of the store which is the global state with the help of it we can access the state from anywhere in the app
 import { createStore, applyMiddleware, compose } from 'redux';  //Improved readability and convenience are the main advantages of using compose.
 import thunk from 'redux-thunk';
